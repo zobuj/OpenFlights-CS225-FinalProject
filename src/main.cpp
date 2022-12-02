d094e517bacf21f311a6d252fe36ac377a42043e
zobuj <lbujalil@gmail.com>
2022-12-02 04:33:55 +0000

    adding website dir

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,15 @@
 #include <iostream>
 using namespace std;
 // change this to your own paths
-<<<<<<< HEAD
+
 string getRoutesPath() {return "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/raw/routes.dat";}
 //string getRoutesPath() {return "/Users/alexisserrano/Documents/CS225_FinalProject/OpenFlights-CS225-FinalProject/data/sample/sampleroutes.dat";}
 
 
 string getAirportsPath() {return  "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/raw/airports.dat";}
 //string getAirportsPath() {return  "/Users/alexisserrano/Documents/CS225_FinalProject/OpenFlights-CS225-FinalProject/data/sample/sampleairports.dat";}
-=======
-//string getRoutesPath() {return "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/raw/routes.dat";}
-string getRoutesPath() {return "/Users/aryanmalhotra/Desktop/cs225project/OpenFlights-CS225-FinalProject/data/sample/sampleroutesdijkstras.dat";}
 
 
-//string getAirportsPath() {return  "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/raw/airports.dat";}
-string getAirportsPath() {return  "/Users/aryanmalhotra/Desktop/cs225project/OpenFlights-CS225-FinalProject/data/sample/sampleairportsdijkstras.dat";}
->>>>>>> origin
 
 int main() {
 
