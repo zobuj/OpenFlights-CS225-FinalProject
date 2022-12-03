b5737c4558ba01dbec54bdb7b03e17072745f921
zobuj <lbujalil@gmail.com>
2022-12-03 00:04:56 +0000

    edited adjacency list

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 // change this to your own paths
 
-string getRoutesPath() {return "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/raw/routes.dat";}
+string getRoutesPath() {return "../OpenFlights-CS225-FinalProject/data/sample/sampleroutes.dat";}
 //string getRoutesPath() {return "/Users/alexisserrano/Documents/CS225_FinalProject/OpenFlights-CS225-FinalProject/data/sample/sampleroutes.dat";}
 
 
-string getAirportsPath() {return  "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/raw/airports.dat";}
+string getAirportsPath() {return  "../OpenFlights-CS225-FinalProject/data/sample/sampleairports.dat";}
 //string getAirportsPath() {return  "/Users/alexisserrano/Documents/CS225_FinalProject/OpenFlights-CS225-FinalProject/data/sample/sampleairports.dat";}
 
 
@@ -23,16 +23,16 @@ int main() {
     Project init(getRoutesPath(),getAirportsPath());
 
     
-    // init.printMap();
-    
-    init.savePNG("presenting");
+    init.printMap();
+    //init.printCoord();
+    init.savePNG("test");
     // should get output that matches DISCORD SCREENSHOT
 
     
     //std::cout << "connection between 7 and 3: " + to_string(init.DFS(7, 3)) << std::endl;
     //init.printConnected(9,"DFS");
-    init.createEdgeWeights();
-    cout << "Shortest Path from Aamir's Airport to Zo_2 Airport: " << init.shortestPath(1, 6) << endl;
+    //init.createEdgeWeights();
+    //cout << "Shortest Path from Aamir's Airport to Zo_2 Airport: " << init.shortestPath(1, 6) << endl;
 
     return 0;
 }
