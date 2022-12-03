9c0f0b3fe2c5f42b0840712241887402e913b0d9
Alexis Serrano <aserrano031@github.com>
2022-12-03 14:51:03 -0600

    updating test.h file

--- includes/tests.h
+++ includes/tests.h
@@ -7,23 +7,5 @@ void ReadRoutesSimple();
 void ReadAirportsSimple();
 void TestsAdjacencyLists();
 void testSimpleGraphOutput();
-string getRoutesPath() {
-    // just change this to your path
-<<<<<<< HEAD
-
-    return "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/sample/sampleroutes.dat";
-}
-string getAirportsPath() {
-    // just change this to your path
-    return  "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/sample/sampleairports.dat";
-
-    
-}
-=======
-    return "/Users/aryanmalhotra/Desktop/cs225project/OpenFlights-CS225-FinalProject/data/sample/sampleroutes.dat";
-}
-string getAirportsPath() {
-    // just change this to your path
-    return  "/Users/aryanmalhotra/Desktop/cs225project/OpenFlights-CS225-FinalProject/data/sample/sampleairports.dat";
-}
->>>>>>> origin
+string getRoutesPath() {return "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/sample/sampleroutes.dat";}
+string getAirportsPath() {return  "/workspaces/cs225/release-f22/release-f22/Final Project/OpenFlights-CS225-FinalProject/data/sample/sampleairports.dat";}
